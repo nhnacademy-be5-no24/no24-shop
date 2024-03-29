016ff6ae21f0c5af7045477a8440b699f55a41cf
jinjiwon <jinjw5441@gmail.com>
2024-03-29 10:39:20 +0900

    feat: Customer Entity 추가

---
+++ src/main/java/com/nhnacademy/shop/customer/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.shop.customer.repository;
+
+import com.nhnacademy.shop.customer.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
