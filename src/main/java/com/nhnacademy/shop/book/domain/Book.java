3d1c71f291c3f5094f96972cc5e15df6d11b2cf1
zezeon <jjaewon527@gmail.com>
2024-04-01 11:44:24 +0900

    add : book restAPI

--- src/main/java/com/nhnacademy/shop/book/domain/Book.java
+++ src/main/java/com/nhnacademy/shop/book/domain/Book.java
@@ -1,58 +1,81 @@
 package com.nhnacademy.shop.book.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.nhnacademy.shop.author.domain.Author;
+import com.nhnacademy.shop.category.domain.Category;
+import com.nhnacademy.shop.tag.domain.Tag;
 import lombok.*;
-import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
+/**
+ * 도서관리 Entity
+ *
+ * @author : 이재원
+ * @date : 2024-03-27
+ */
+@Entity
+@Getter
+@EqualsAndHashCode
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Getter
-@Entity
-@Table(name = "book")
 public class Book {
+
     @Id
-    @Column(name = "book_isbn")
+    @Column(name = "book_isbn", nullable = false)
     private String bookIsbn;
 
-    @Column(name = "book_title")
+    @Column(name = "book_title", nullable = false)
+    @Length(max = 255)
     private String bookTitle;
 
-    @Column(name = "book_desc")
+    @Column(name = "book_desc", nullable = false)
     private String bookDesc;
 
-    @Column(name = "book_publisher")
+    @Column(name = "book_publisher", nullable = false)
     private String bookPublisher;
 
-    @Column(name = "book_publish_at")
-    private LocalDate bookPublishAt;
+    @Column(name = "book_publisher_at", nullable = false)
+    private LocalDate bookPublisherAt;
 
-    @Column(name = "book_fixed_price")
-    private Long bookFixedPrice;
+    @Column(name = "book_fixed_price", nullable = false)
+    private int bookFixedPrice;
 
-    @Column(name = "book_sale_price")
-    private Long bookSalePrice;
+    @Column(name = "book_sale_price", nullable = false)
+    private int bookSalePrice;
 
-    @Column(name = "book_is_packing")
+    @Column(name = "book_is_packing", nullable = false)
     private boolean bookIsPacking;
 
-    @Column(name = "book_views")
+    @Column(name = "book_views", nullable = false)
     private Long bookViews;
 
-    @Column(name = "book_status")
-    private Integer bookStatus;
+    /*
+    Express "book_status" to integer
+    0 : 판매중
+    1 : 수량부족
+    2 : 판매종료
+    3 : 삭제된 도서
+     */
+    @Column(name = "book_status", nullable = false)
+    private int bookStatus;
 
-    @Column(name = "book_quantity")
-    private Long bookQuantity;
+    @Column(name = "book_quantity", nullable = false)
+    private int bookQuantity;
 
     @Column(name = "book_image")
     private String bookImage;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Tag> tags;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Category> categories;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Author> authors;
+
 }
--- src/main/java/com/nhnacademy/shop/book/repository/BookRepository.java
+++ src/main/java/com/nhnacademy/shop/book/repository/BookRepository.java
@@ -3,5 +3,22 @@ package com.nhnacademy.shop.book.repository;
 import com.nhnacademy.shop.book.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+/**
+ * 도서관리 repository
+ *
+ * @author : 이재원
+ * @date : 2024-03-27
+ */
 public interface BookRepository extends JpaRepository<Book, String> {
+
+    List<Book> getAllBooks();
+
+    List<Book> findByName(String bookName);
+
+    Book findByBookIsbn(String bookIsbn);
+
+    List<Book> findByBookDesc(String bookDesc);
+
 }
