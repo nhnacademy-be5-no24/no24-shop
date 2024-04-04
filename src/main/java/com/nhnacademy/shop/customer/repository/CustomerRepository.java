420c7b003e790c029531126854a61d707bd0911b
jinjiwon <jinjw5441@gmail.com>
2024-04-04 11:03:25 +0900

    fix: Customer Repository method 수정

--- src/main/java/com/nhnacademy/shop/customer/repository/CustomerRepository.java
+++ src/main/java/com/nhnacademy/shop/customer/repository/CustomerRepository.java
@@ -4,5 +4,4 @@ import com.nhnacademy.shop.customer.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer findByCustomerNo(Long customerNo);
 }
