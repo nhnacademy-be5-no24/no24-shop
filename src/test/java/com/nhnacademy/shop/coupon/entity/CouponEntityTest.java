bddf59202cb49027b7daf04df19e7bf5c8b5e86b
BHwi <bhwi0422@gmail.com>
2024-04-18 19:02:48 +0900

    add: coupon

---
+++ src/test/java/com/nhnacademy/shop/coupon/entity/CouponEntityTest.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.shop.coupon.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 설명
+ *
+ * @Author : 박병휘
+ * @Date : 2024/04/18
+ */
+class CouponTest {
+
+}
