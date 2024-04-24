de0f8901f67d298cbf6438050e3629c362714308
jinjiwon <jinjw5441@gmail.com>
2024-04-24 15:24:32 +0900

    fix: Cart Id customerNo로 변경

--- src/main/java/com/nhnacademy/shop/cart/exception/CartNotFoundException.java
+++ src/main/java/com/nhnacademy/shop/cart/exception/CartNotFoundException.java
@@ -1,7 +1,7 @@
 package com.nhnacademy.shop.cart.exception;
 
 public class CartNotFoundException extends RuntimeException {
-    public CartNotFoundException(String customerId) {
-        super(customerId + "의 장바구니가 존재하지 않습니다.");
+    public CartNotFoundException(Long customerNo) {
+        super(customerNo + "의 장바구니가 존재하지 않습니다.");
     }
 }
