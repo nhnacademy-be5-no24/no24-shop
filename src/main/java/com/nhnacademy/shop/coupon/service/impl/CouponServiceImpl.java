35e1da0b0441cb27120298b1e36d0425d76bcd18
BHwi <bhwi0422@gmail.com>
2024-05-16 13:09:46 +0900

    mod: dbcp config

--- src/main/java/com/nhnacademy/shop/coupon/service/impl/CouponServiceImpl.java
+++ src/main/java/com/nhnacademy/shop/coupon/service/impl/CouponServiceImpl.java
@@ -21,6 +21,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -74,6 +75,7 @@ public class CouponServiceImpl implements CouponService {
 
         List<CouponResponseDto> couponList = couponRepository.findAll().stream()
                 .filter(coupon -> !couponReceiveMap.containsKey(coupon.getCouponId()) || couponReceiveMap.get(coupon.getCouponId()) < coupon.getIssueLimit())
+                .filter(coupon -> coupon.getDeadline().atStartOfDay().isAfter(LocalDateTime.now()))
                 .map(coupon -> {
                     CouponResponseDto couponResponseDto = CouponResponseDto.builder()
                             .couponId(coupon.getCouponId())
